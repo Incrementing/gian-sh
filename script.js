dae51078b03a11da86a5c0d656b96ff8dab0d5f8
incrementing <hello@gian.sh>
2021-04-11 14:56:36 +0100

    Spotify Web API

--- script.js
+++ script.js
@@ -2,4 +2,24 @@ $(function () {
     console.log("Hello %c:)", "color:cyan;");
 
     // Get Spotify Now Playing from https://np.gian.sh
+    $.ajax({
+        method: "GET",
+        url: "https://np.gian.sh",
+    }).done((resp, status) => {
+        if (status === "success") {
+            $("#spotify-art").find("img").attr("src", resp.albumArt);
+            $("#spotify-art").fadeIn('fast');
+
+            $("#spotify-np").html("<a href='" + resp.link + "' target='_blank'>" + resp.name + "</a><br>" + resp.album + "<br>" + resp.artist);
+            $("#spotify-age").html((resp.age.toFixed(2) + "s").replaceAll("0.00s", "now"))
+        }
+    }).fail((resp, status) => {
+        let trace = JSON.stringify(resp);
+        if (status === 'abort') {
+            trace = "The XHR request was aborted by the browser.";
+        } else {
+            trace = status + ": " + resp.responseText;
+        }
+        console.warn("Failed to get Now Playing:", trace);
+    });
 });
